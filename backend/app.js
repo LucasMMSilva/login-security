d1be93db6d4f63d49a47e91d7b809a1d5568e945
Your Name <86989674+LucasMMSilva@users.noreply.github.com>
2023-11-23 21:21:14 -0300

    configurando o cors

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,17 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const router = require('./routes/routes')
+
+const cors = require('cors')
+const router = require('./routes/routes')
+
+app.use(cors({credentials:true,origin:'http://localhost:3000'}))
+
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
 
 app.use('/',router)
 
