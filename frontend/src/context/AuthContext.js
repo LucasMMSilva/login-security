2b7843198a552deb263a255111c8b2019ce8705f
Your Name <86989674+LucasMMSilva@users.noreply.github.com>
2023-11-27 13:55:23 -0300

    context de autenticação

---
+++ frontend/src/context/AuthContext.js
@@ -0,0 +1,14 @@
+import {createContext,useState} from 'react'
+
+export const AuthContext = createContext()
+export const AuthContextProvider = (({children})=>{
+    const token = localStorage.getItem('token')
+    const hasToken = token ? true : false
+    const [authenticated, setAuthenticated] = useState(hasToken)
+
+    return(
+        <AuthContext.Provider value={{authenticated,setAuthenticated}}>
+            {children}
+        </AuthContext.Provider>
+    )
+})
