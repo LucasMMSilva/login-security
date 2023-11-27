fecb32207a58213dbaf14fe26f640044d8ab723c
Your Name <86989674+LucasMMSilva@users.noreply.github.com>
2023-11-27 16:21:46 -0300

    login frontend

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import {useState} from 'react'
 import {Link} from 'react-router-dom'
+import { useAuthentication } from '../../hooks/useAuthentication'
 const Login = () => {
+  
+  const {login} = useAuthentication()
+  
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+
+    const user = {
+      email,password
+    }
+
+    login(user)
+
+  }
+  
   return (
+    
     <div className="formContainer">
         <h1>Login</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
             <label htmlFor="">E-mail:</label>
-            <input type="text" />
+            <input onChange={(e)=>setEmail(e.target.value)} type="text" value={email}/>
             <label htmlFor="">Password:</label>
-            <input type="password" />
+            <input onChange={(e)=>setPassword(e.target.value)} type="password" value={password}/>
             <button type="submit">Login</button>
         </form>
         <p>Not have an account yet? <Link to='/register'>Click here</Link>.</p>
