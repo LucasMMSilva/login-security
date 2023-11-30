491fe57c2ccbc0a4e7b156672e54532f91040ebc
Your Name <86989674+LucasMMSilva@users.noreply.github.com>
2023-11-29 22:21:10 -0300

    removeLog

--- backend/controller/LogAuthenticationController.js
+++ backend/controller/LogAuthenticationController.js
@@ -1,13 +1,17 @@
 const LogAuthentication = require("../models/LogAuthentication")
+const { deleteOne } = require("../models/User")
 
-const saveUserAndToken = (userId,token) => {
+const saveUserAndToken = async (userId,token) => {
     const log = new LogAuthentication({
         userId,
         token,
         expireAt: new Date(Date.now() + 86400000)
     })
-    const userLog = log.save()
+    const userLog = await log.save()
     return userLog 
 }
+const removeLogToken = async (userId)=>{
+    await LogAuthentication.deleteOne(userId)
+}
 
-module.exports = {saveUserAndToken}
+module.exports = {saveUserAndToken,removeLogToken}
